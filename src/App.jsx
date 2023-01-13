531644c5d0b3e69b6a5e257f641dabf604d7ead8
Gustavo Silva <contatogustavosilva017@outlook.com>
2023-01-13 02:21:38 -0300

    Initial Setup [ Finished ✓ ]
    BreakPoint [ ⌀ ]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='bg-slate-100 w-screen h-screen text-center'>
+      <h1 className='mb-[100px]'>Hello React <span className="bg-[hsl(278,68%,11%)] text-white px-[5px] py-[2px] rounded">(Vite)</span> + Tailwind</h1>
+
+      <div />
+    </div>
+  )
+}
+
+export default App
